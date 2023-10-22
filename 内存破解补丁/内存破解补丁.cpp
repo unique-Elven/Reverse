bdee3168a257e295936a9a11fb55d8050d2a0566
unique-Elven <2109258617@qq.com>
2023-10-22 15:15:21 +0800

    添加项目文件。

---
+++ 内存破解补丁/内存破解补丁.cpp
@@ -0,0 +1,16 @@
+﻿// 内存破解补丁.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <windows.h>
+#include <iostream>
+
+int main()
+{
+    int pId;
+    printf("请输入要破解的程序进程ID：");
+    scanf("%d", &pId);
+    byte buff[] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
+    HANDLE hHandle=OpenProcess(PROCESS_ALL_ACCESS, TRUE, pId);
+    WriteProcessMemory(hHandle, (LPVOID)0x004010FD, buff, 6, NULL);
+}
+
+
